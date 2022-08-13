9e9424243b393edc3cee432d29c69db6472ec96e
Manivannan11a <manivannan11a@gmail.com>
2022-08-13 21:38:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './component/layout';
+import React, { Suspense } from 'react';
+
+const SamplePage = React.lazy(() => import('./component/sample-page'));
+
+const Loading = ({ children }) => (
+  <Suspense fallback={<h1>Loading</h1>}>
+    {children}
+  </Suspense>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/app" element={<Loading><SamplePage /></Loading>} />
+          <Route path="*" element={<Loading><h1>Not Found</h1></Loading>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/sample-page/index.js
@@ -0,0 +1,7 @@
+const SamplePage = () => {
+  return (
+    <h1>Smaple Page</h1>
+  )
+}
+
+export default SamplePage;
